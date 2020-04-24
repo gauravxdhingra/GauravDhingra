0afbf853a6aa4e4359571f02b376f277764a09d8
GauravXd <grvdhingra1999@gmail.com>
2020-04-24 19:06:20 +0530

    Visually Complete with buttons

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:github_portfolio/responsive_widget.dart';
+import 'dart:html' as html;
+import 'package:html/dom.dart' as html;
+import 'package:url_launcher/url_launcher.dart';
 
 class ProfilePage extends StatelessWidget {
   @override
@@ -7,10 +10,19 @@ class ProfilePage extends StatelessWidget {
     return ResponsiveWidget(
       largeScreen: Scaffold(
         backgroundColor: Colors.black,
-        appBar: AppBar(
-          elevation: 0,
-          backgroundColor: Colors.black,
-        ),
+        appBar: ResponsiveWidget.isSmallScreen(context)
+            ? AppBar(
+                title: Text(
+                  'gauravxdhingra',
+                  textScaleFactor: 2,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                elevation: 0,
+                backgroundColor: Colors.black,
+              )
+            : null,
         drawer: ResponsiveWidget.isSmallScreen(context)
             ? Drawer(
                 child: ListView(
@@ -23,7 +35,6 @@ class ProfilePage extends StatelessWidget {
           child: AnimatedPadding(
             padding: EdgeInsets.all(
               MediaQuery.of(context).size.height * 0.1,
-
             ),
             child: ResponsiveWidget(
               largeScreen: Column(
@@ -40,10 +51,308 @@ class ProfilePage extends StatelessWidget {
                   SocialInfo(),
                 ],
               ),
-            ),,
+            ),
             duration: Duration(seconds: 1),
           ),
         ),
+      ),
+    );
+  }
+
+  List<Widget> navButtons() => [
+        // NavButton(
+        //   text: "about",
+        //   onPressed: () {},
+        // ),
+        // NavButton(
+        //   text: "work",
+        //   onPressed: () {},
+        // ),
+        // NavButton(
+        //   text: "contact",
+        //   onPressed: () {},
+        // ),
+      ];
+}
+
+class NavHeader extends StatelessWidget {
+  final List<Widget> navButtons;
+
+  const NavHeader({Key key, this.navButtons}) : super(key: key);
+
+  Widget build(BuildContext context) {
+    return ResponsiveWidget(
+      largeScreen: Row(
+        mainAxisAlignment: ResponsiveWidget.isSmallScreen(context)
+            ? MainAxisAlignment.center
+            : MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          if (!ResponsiveWidget.isSmallScreen(context)) Gaurav(),
+//          Spacer(),
+          if (!ResponsiveWidget.isSmallScreen(context))
+            Row(
+              children: navButtons,
+            )
+        ],
+      ),
+    );
+  }
+}
+
+class Gaurav extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Text(
+          "GD",
+          textScaleFactor: 2,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(
+          width: 5,
+        ),
+        AnimatedContainer(
+          duration: Duration(seconds: 1),
+          height: 8,
+          width: 8,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: Colors.orange,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class NavButton extends StatelessWidget {
+  final text;
+  final onPressed;
+  final Color color;
+
+  const NavButton(
+      {Key key,
+      @required this.text,
+      @required this.onPressed,
+      this.color = Colors.orange})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return OutlineButton(
+      child: Text(text),
+      borderSide: BorderSide(
+        color: color,
+      ),
+      onPressed: onPressed,
+      highlightedBorderColor: color,
+    );
+  }
+}
+
+class ProfileInfo extends StatelessWidget {
+  profileImage(context) => Container(
+        height: ResponsiveWidget.isSmallScreen(context)
+            ? MediaQuery.of(context).size.height * 0.25
+            : MediaQuery.of(context).size.width * 0.25,
+        width: ResponsiveWidget.isSmallScreen(context)
+            ? MediaQuery.of(context).size.height * 0.25
+            : MediaQuery.of(context).size.width * 0.25,
+        decoration: BoxDecoration(
+          backgroundBlendMode: BlendMode.luminosity,
+          color: Colors.deepOrange,
+//            borderRadius: BorderRadius.circular(40),
+          shape: BoxShape.circle,
+          image: DecorationImage(
+            image: NetworkImage(
+              "https://scontent.fdel1-4.fna.fbcdn.net/v/t1.0-9/p960x960/53792671_431528817654025_7064724171124637696_o.jpg?_nc_cat=100&_nc_sid=85a577&_nc_ohc=oHwArTjA6hIAX9JIpXL&_nc_ht=scontent.fdel1-4.fna&_nc_tp=6&oh=2ab00406a8d35258299842f7256c91d7&oe=5EC73D5B",
+            ),
+            alignment: Alignment.center,
+            fit: BoxFit.cover,
+          ),
+        ),
+      );
+
+  final profileData = Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      Text(
+        "Hi there! My name is",
+        textScaleFactor: 2,
+        style: TextStyle(color: Colors.orange),
+      ),
+      Text(
+        "Gaurav\nDhingra",
+        textScaleFactor: 5,
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      SizedBox(
+        height: 10,
+      ),
+      Text(
+        "A Student At Bhagwan Parshuram Institute of Technology, \nNew Delhi (IN).\n\n",
+        softWrap: true,
+        textScaleFactor: 1.5,
+        overflow: TextOverflow.visible,
+        style: TextStyle(color: Colors.white70),
+      ),
+      Container(
+        height: 220,
+        width: 500,
+        child: Text(
+          "An Innovative and Enthusiastic Developer with a drive to build "
+          "useful software for the people and the community, carrying a strong "
+          "sense of Stunning User Interface and a Delightful User Experience to "
+          "make the product highly intuitive and easy to use for the end user",
+          softWrap: true,
+          overflow: TextOverflow.visible,
+          textScaleFactor: 1.5,
+          style: TextStyle(color: Colors.white70),
+        ),
+      ),
+      SizedBox(
+        height: 20,
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          RaisedButton(
+            shape: StadiumBorder(),
+            child: Text("Resume"),
+            color: Colors.red,
+            onPressed: () {},
+            padding: EdgeInsets.all(10),
+          ),
+          SizedBox(
+            width: 20,
+          ),
+          OutlineButton(
+            borderSide: BorderSide(
+              color: Colors.red,
+            ),
+            shape: StadiumBorder(),
+            child: Text("Say Hi!"),
+            color: Colors.red,
+            onPressed: () {},
+            padding: EdgeInsets.all(10),
+          )
+        ],
+      )
+    ],
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return ResponsiveWidget(
+      largeScreen: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[profileImage(context), profileData],
+      ),
+      smallScreen: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          profileImage(context),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.1,
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 15),
+            child: profileData,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class SocialInfo extends StatelessWidget {
+  List<Widget> socialMediaWidgets() {
+    return [
+      NavButton(
+        text: "GitHub",
+        onPressed: () {
+          html.window.open("https://github.com/gauravxdhingra", "Git");
+        },
+        color: Colors.blue,
+      ),
+      NavButton(
+        text: "LinkedIn",
+        onPressed: () {
+          html.window.open("https://www.linkedin.com/in/gauravxdhingra/", "Fb");
+        },
+        color: Colors.blue,
+      ),
+      NavButton(
+        text: "Instagram",
+        onPressed: () {
+          html.window
+              .open("https://www.instagram.com/gauravxdhingra/", "Twitter");
+        },
+        color: Colors.blue,
+      ),
+      NavButton(
+        text: "Facebook",
+        onPressed: () {
+          html.window.open("https://www.facebook.com/gauravxdhingra", "Fb");
+        },
+        color: Colors.blue,
+      ),
+    ];
+  }
+
+  Widget copyRightText() => Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              "Made with ❤ using Flutter",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.grey,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              "Gaurav Dhingra ©️2020",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.grey,
+              ),
+            ),
+          ),
+        ],
+      );
+
+  @override
+  Widget build(BuildContext context) {
+    return ResponsiveWidget(
+      largeScreen: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: socialMediaWidgets(),
+          ),
+          copyRightText(),
+        ],
+      ),
+      smallScreen: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          ...socialMediaWidgets(),
+          copyRightText(),
+        ],
       ),
     );
   }
